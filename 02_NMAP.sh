0665c18120e181ac4db6f2eb6e987dd3acb388b5
marcelobrazolim <tchetchelo01@gmail.com>
2022-01-12 11:43:44 -0300

    a

--- 02_NMAP.sh
+++ 02_NMAP.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-(bash ~/scripts/map.sh -s -oN 'nmap')
-(bash ~/scripts/nmap-mass.sh -sV -sC -Pn -sU -vv)
+(bash ../scripts/map.sh -s -oN 'nmap')
+(bash ../scripts/nmap-mass.sh -sV -sC -Pn -sU -vv)
 read -n 1 -s
