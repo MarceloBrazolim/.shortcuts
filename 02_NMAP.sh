983de128e0396b337ec4512a59ca487387fe8278
Marcelo Brazolim <marcelobrazolim@gmail.com>
2022-01-14 17:39:31 -0300

    a

--- 02_NMAP.sh
+++ 02_NMAP.sh
@@ -2,5 +2,6 @@
 (bash ../scripts/map.sh -wr)
 (bash ../scripts/map.sh -ip)
 (bash ../scripts/map.sh -nW -s -oN 'nmap')
+echo ""
 (bash ../scripts/nmap-mass.sh -nW -sV -sC -Pn -sU -vv -s -oN nmap_log)
 read -n 1 -s
